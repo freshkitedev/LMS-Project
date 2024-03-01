25d49635573b24c38fec48a2f5b85915bc3473f8
shilpa053020 <shilpa053020@gmail.com>
2024-03-01 10:25:44 +0530

    ID : 104 Issue : Create Notification && Order Model

---
+++ Server/models/notificationModel.ts
@@ -0,0 +1,33 @@
+import mongoose, { Document, Schema, Model } from "mongoose";
+
+export interface INotification extends Document {
+  title: string;
+  message: string;
+  status: string;
+  userId: string;
+}
+
+const notificationSchema = new Schema<INotification>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "unread",
+    }
+  },
+  { timestamps: true }
+);
+
+const NotificationModel: Model<INotification> = mongoose.model(
+  "notification",
+  notificationSchema
+);
+export default NotificationModel;
