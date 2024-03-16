a6d1c74b966695873c18406788b697f42cc460e5
Vishva77 <vishvavikki77@gmail.com>
2024-03-16 10:54:17 +0530

    loginUser and LogoutUser completed

--- Server/routes/userRoute.ts
+++ Server/routes/userRoute.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { Activation, userRegistration } from "../controllers/userController";
+import { Activation, loginUser, logoutUser, userRegistration } from "../controllers/userController";
 const router = express.Router();
 
 router.post("/registration", userRegistration);
 
 router.post("/activateCode", Activation);
+
+router.post("/loginUser", loginUser);
+
+router.get("/logoutUser", logoutUser);
 
 export default router;
